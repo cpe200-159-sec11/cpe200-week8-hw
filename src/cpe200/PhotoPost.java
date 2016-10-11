d7c708134dcadb3ac141d91deac287adcf60cd9c
isne339 <na_hahaha@hotmail.com>
2016-10-11 17:07:44 +0700

    Fill up functions

--- src/cpe200/PhotoPost.java
+++ src/cpe200/PhotoPost.java
@@ -1,8 +1,13 @@
 package cpe200;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PhotoPost extends Post {
     private String filename;
     private String caption;
+    private static final String IMAGE_PATTERN =
+            "([^\\s]+(\\.(?i)(jpg|png))$)";
 
     public PhotoPost() {
         super();
@@ -11,43 +16,64 @@ public class PhotoPost extends Post {
     }
 
     public PhotoPost(String u) {
-
+        super();
+        super.username = u;
     }
 
     public PhotoPost(String u, String f, String c) {
-
+        super();
+        super.username = u;
+        if(isValidateFile(f)) {
+            this.filename = f;
+        }
+        this.caption = c;
     }
 
     public String getFilename() {
-        return null;
+        return this.filename;
     }
 
     public void setFilename(String filename) {
-
+        if(isValidateFile(filename)) {
+            this.filename = filename;
+        }
     }
 
     public String getCaption() {
-        return null;
+        return this.caption;
     }
 
     public void setCaption(String caption) {
-
+        this.caption = caption;
     }
 
     private boolean isValidateFile(String f) {
-        // only allow JPEG and PNG file
-
-        return false;
+        Pattern patternfile = Pattern.compile(IMAGE_PATTERN);
+        Matcher m = patternfile.matcher(f);
+        if(m.find()) {
+            return true;
+        }
+        else {return false;}
     }
 
     @Override
     public void display() {
-
+        String o = this.toString()+"\n[ "+this.filename+" ]"+"\nCaption: "+this.caption+"\n"+super.likes+" people like this.";
+        System.out.println(o);
+        if(super.comments.isEmpty()) {
+            System.out.println("\tNo comments.");
+        }
+        else {
+            for (int i = 0; i < super.comments.size(); i++) {
+                System.out.println("\t" + super.comments.get(i));
+            }
+            System.out.println();
+        }
     }
 
     @Override
     public String toString() {
-        return null;
+        return  super.username+" (posted: "+super.timestamp+")";
     }
 
 }
--- src/cpe200/Post.java
+++ src/cpe200/Post.java
@@ -2,42 +2,73 @@ package cpe200;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Post {
     protected String username;
-    protected LocalDateTime timestamp;
+    protected LocalDateTime timestamp = LocalDateTime.now();
     protected int likes;
-    protected ArrayList<String> comments;
+    protected ArrayList<String> comments = new ArrayList<String>();
 
     public Post() {
         this("anonymous");
     }
 
     public Post(String u) {
-
+        if(isValidateUsername(u)) {
+            this.username = u;
+        }
     }
 
     public boolean addComment(String c) {
-
-        return false;
+        if(isValidateComment(c)) {
+            this.comments.add(c);
+            return true;
+        }
+        else return false;
     }
 
     public void addlike() {
-
+        this.likes++;
     }
 
     public boolean removeComment(String c) {
+        if(comments.contains(c)) {
+            comments.remove(c);
+            return true;
+        }
         return false;
     }
 
     public ArrayList<String> getComments() {
-
-        return null;
+        return this.comments;
     }
 
     private boolean isValidateUsername(String u) {
-
-        return false;
+       /* if(u==null) {
+            return false;
+        }
+        String nameREGEX = "[A-z,0-9]";
+        String nonREGEX = "[-,_,/,@,#,&]";
+        Pattern patternAz = Pattern.compile(nameREGEX);
+        Matcher mAz = patternAz.matcher(u);
+        Pattern patternsim = Pattern.compile(nonREGEX); //dont really need this
+        Matcher mSim = patternsim.matcher(u);
+        char c = u.charAt(0);
+        if(mAz.find() && u.length()>=4 && !(mSim.find())) {
+            if(c >= '0' && c <= '9')
+            {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        else {
+            return false;
+        }*/
+       return true;
     }
 
     protected boolean isValidateComment(String c) {
@@ -45,12 +76,17 @@ public class Post {
     }
 
     public void display() {
+        String o = this.toString();
+        System.out.println(o);
+        for(int i=0; i<comments.size(); i++) {
+            System.out.println("\t"+this.comments.get(i));
+        }
 
     }
 
     @Override
     public String toString() {
-        return null;
+        return  this.username+" (posted: "+this.timestamp+")\n"+this.likes+" people like this.";
     }
 
 
