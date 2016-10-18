056a68c7bdc0aaf5dd649e15c9cd7266c7c2fa81
Panpech Pothong <PanpechMac@Panpechs-MacBook-Pro.local>
2016-10-18 13:34:07 +0700

    Add function for each class

--- src/cpe200/MessagePost.java
+++ src/cpe200/MessagePost.java
@@ -9,20 +9,35 @@ public class MessagePost extends Post {
     }
 
     public MessagePost(String u) {
+        super();
+        super.username = u;
 
     }
 
     public MessagePost(String u, String m) {
-
+        super();
+        super.username = u;
+        this.message = m;
     }
 
     @Override
     public void display() {
+        String o = this.toString()+"\nMessage: "+ this.message + "\n" + super.likes +" people like this.";
+        System.out.println(o);
+        if (super.comments.isEmpty())
+        {
+            System.out.print("\tNo comments.");
+        }
+        else {
+            for (int i = 0; i < super.comments.size(); i++)
+                System.out.println("\t" + super.comments.get(i));
+        }
+        System.out.println();
 
     }
 
     @Override
     public String toString() {
-        return null;
+        return super.username + "(postes: "+super.timestamp+")";
     }
 }
